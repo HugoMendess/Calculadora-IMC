c61b12714275a7691a2df84a57d4b32d70cb7159
Hugo Mendes <hugo-vmendes@hotmail.com>
2020-10-04 21:16:54 -0300

    Criando função para pegar os dados

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-formula = peso/altura*altura
+// formula = peso/altura*altura
+
+
+
+  function isNumber(value) {
+
+        return /^\d+(?:\.\d+)?$/.test(value);
+
+   };
+function getValue(){
+ 	var altura = document.getElementById('content-altura-campo').value;	
+ 	var peso = document.getElementById('content-peso-campo').value;
+ 	console.log(altura);
+ 	console.log(peso);
+
+ 	if(altura == '' && peso == ''){
+ 		alert('Informe valores validos');
+ 	}
+
+ 	else{
+
+ 		if(isNumber(altura) && isNumber(peso)){
+ 			
+ 		}
+
+ 		else{
+ 			alert("Incoreto - Utilize ponto(Exemplo: 1.89)");
+ 		}
+
+ 	}
+}
